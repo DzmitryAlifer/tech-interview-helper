daf648e7ccba8fae90315ec597877fe6d6feec23
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-10-08 15:02:29 +0200

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { VoiceRecognitionService } from './service/voice-recognition.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import { VoiceRecognitionService } from './service/voice-recognition.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  text = this.voiceRecognitionService.text;
+  readonly answers$: Observable<string[]> = this.voiceRecognitionService.getAnswers();
 
   constructor(private readonly voiceRecognitionService : VoiceRecognitionService) {
     this.voiceRecognitionService.init();
--- src/app/service/voice-recognition.service.ts
+++ src/app/service/voice-recognition.service.ts
@@ -1,34 +1,35 @@
 import { Injectable } from '@angular/core';
-import { JS_DICTIONARY } from '../../dictionary/js'; 
+import { JS_DICTIONARY } from '../../dictionary/js';
+import { fromEvent, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 declare var webkitSpeechRecognition: any
 
 @Injectable({providedIn: 'root'})
 export class VoiceRecognitionService {
-  recognition =  new webkitSpeechRecognition();
-  
   isStopped = false;
-  public text = '';
-  tempWords = '';
+  recognition =  new webkitSpeechRecognition();
 
-  constructor() { }
+  private readonly answers$: Observable<string[]> = fromEvent(this.recognition, 'result').pipe(
+    map(({results}: any) => Array.from(results)[0]),
+    filter((result: any) => result.isFinal),
+    map((result: any) => result[0].transcript.split(' ')),
+    map(pronouncedWords => Array.from(JS_DICTIONARY.values())
+        .filter(dictionaryAnswer => hasMatchedWord(pronouncedWords, dictionaryAnswer.dictionary))
+        .map(({answer}) => answer)),
+  );
 
   init() {
     this.recognition.interimResults = true;
     this.recognition.lang = 'en-US';
 
-    this.recognition.addEventListener('result', (e:any) => {
-      const transcript = Array.from(e.results)
-        .map((result:any) => result[0])
-        .map((result) => result.transcript)
-        .join('');
-      this.tempWords = transcript;
-      const pronouncedWords = transcript.split(' ');
-      const matchedAnswers: string[] = Array.from(JS_DICTIONARY.values())
-        .filter(dictionaryAnswer => hasMatchedWord(pronouncedWords, dictionaryAnswer.dictionary))
-        .map(({answer}) => answer);
-      console.log(matchedAnswers);
+    this.answers$.subscribe(answers => {
+      console.log(answers);
     });
+  }
+
+  getAnswers(): Observable<string[]> {
+    return this.answers$;
   }
 
   start() {
@@ -38,20 +39,13 @@ export class VoiceRecognitionService {
       if (this.isStopped) {
         this.recognition.stop();
       } else {
-        this.wordConcat()
         this.recognition.start();
       }
     });
   }
   stop() {
     this.isStopped = true;
-    this.wordConcat();
     this.recognition.stop();
-  }
-
-  wordConcat() {
-    this.text = this.text + ' ' + this.tempWords + '.';
-    this.tempWords = '';
   }
 }
 
