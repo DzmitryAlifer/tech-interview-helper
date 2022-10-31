d36517c851b510da11c1e6f65080ede2daf2aeed
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-10-31 09:50:46 +0100

    1

--- src/app/knowledge-sidebar/knowledge-sidebar.component.ts
+++ src/app/knowledge-sidebar/knowledge-sidebar.component.ts
@@ -1,9 +1,6 @@
 import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnChanges, ViewChild } from '@angular/core';
 import { Tech } from 'src/types';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { FormGroup, FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { delay, filter, map, tap } from 'rxjs/operators';
+import { delay, filter, map, switchMap, tap } from 'rxjs/operators';
 import { AnswerProviderService } from '../service/answer-provider.service';
 
 
@@ -13,40 +10,9 @@ import { AnswerProviderService } from '../service/answer-provider.service';
   styleUrls: ['./knowledge-sidebar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class KnowledgeSidebar implements OnChanges, AfterViewInit {
+export class KnowledgeSidebar {
   readonly techs = Object.values(Tech);
-  form = new FormGroup({ 'codeUrl': new FormControl(null) });
   readonly allAnswers$ = this.answerProviderService.getAllAnswers();
 
-  readonly safeCodeUrl$: Observable<SafeResourceUrl> =
-    this.form.get('codeUrl')!.valueChanges.pipe(
-      map(codeUrl => this.domSanitizer.bypassSecurityTrustResourceUrl(codeUrl)),
-    );
-
-  constructor(
-    private readonly answerProviderService: AnswerProviderService,
-    private readonly domSanitizer: DomSanitizer, 
-    private readonly elementRef: ElementRef,
-  ) {}
-
-  ngOnChanges() {
-    console.log(document.getElementById('#code'));
-  }
-
-  ngAfterViewInit() {
-    new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        console.log(mutation.type);
-      });
-    }).observe(this.elementRef.nativeElement, { attributes: true, childList: true, characterData: true });
-
-    this.safeCodeUrl$.pipe(
-      filter(Boolean),
-      delay(3000),
-      tap(() => {
-        const iframeElement = document.querySelector<HTMLIFrameElement>("#code");
-        console.log(iframeElement?.contentWindow?.document.body.innerText);
-      }),
-    ).subscribe();
-  }
+  constructor(private readonly answerProviderService: AnswerProviderService) {}
 }
