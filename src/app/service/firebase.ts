17c144f73b7be3fd806d92a2e12ef88dcf546d55
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-12-13 09:16:31 +0100

    1

--- src/app/service/firebase.ts
+++ src/app/service/firebase.ts
@@ -31,13 +31,20 @@ export interface Settings {
 }
 
 const config = {
-  apiKey: 'AIzaSyBxfkwcHwkojE_-lTJpSU0YHqVyxFJmr9s',
-  authDomain: 'tech-interview-helper1.firebaseapp.com',
-  projectId: 'tech-interview-helper1',
-  storageBucket: 'tech-interview-helper1.appspot.com',
-  messagingSenderId: '228304227676',
-  appId: '1:228304227676:web:fb3bbc2fcd70d2a9b5c599',
-  measurementId: 'G-40ZBRQZNTP',
+  // apiKey: 'AIzaSyBxfkwcHwkojE_-lTJpSU0YHqVyxFJmr9s',
+  // authDomain: 'tech-interview-helper1.firebaseapp.com',
+  // projectId: 'tech-interview-helper1',
+  // storageBucket: 'tech-interview-helper1.appspot.com',
+  // messagingSenderId: '228304227676',
+  // appId: '1:228304227676:web:fb3bbc2fcd70d2a9b5c599',
+  // measurementId: 'G-40ZBRQZNTP',
+  apiKey: "AIzaSyBxfkwcHwkojE_-lTJpSU0YHqVyxFJmr9s",
+  authDomain: "tech-interview-helper.firebaseapp.com",
+  projectId: "tech-interview-helper",
+  storageBucket: "tech-interview-helper.appspot.com",
+  messagingSenderId: "228304227676",
+  appId: "1:228304227676:web:085e2ebaf9aa8547b5c599",
+  measurementId: "G-M4891K4VB7"
 };
 
 const app = initializeApp(config);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,20 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyBxfkwcHwkojE_-lTJpSU0YHqVyxFJmr9s',
-    authDomain: 'tech-interview-helper1.firebaseapp.com',
-    projectId: 'tech-interview-helper1',
-    storageBucket: 'tech-interview-helper1.appspot.com',
-    messagingSenderId: '228304227676',
-    appId: '1:228304227676:web:fb3bbc2fcd70d2a9b5c599',
-    measurementId: 'G-40ZBRQZNTP',
+    // apiKey: 'AIzaSyBxfkwcHwkojE_-lTJpSU0YHqVyxFJmr9s',
+    // authDomain: 'tech-interview-helper1.firebaseapp.com',
+    // projectId: 'tech-interview-helper1',
+    // storageBucket: 'tech-interview-helper1.appspot.com',
+    // messagingSenderId: '228304227676',
+    // appId: '1:228304227676:web:fb3bbc2fcd70d2a9b5c599',
+    // measurementId: 'G-40ZBRQZNTP',
+    apiKey: "AIzaSyBxfkwcHwkojE_-lTJpSU0YHqVyxFJmr9s",
+    authDomain: "tech-interview-helper.firebaseapp.com",
+    projectId: "tech-interview-helper",
+    storageBucket: "tech-interview-helper.appspot.com",
+    messagingSenderId: "228304227676",
+    appId: "1:228304227676:web:085e2ebaf9aa8547b5c599",
+    measurementId: "G-M4891K4VB7"
   },
 };
 
