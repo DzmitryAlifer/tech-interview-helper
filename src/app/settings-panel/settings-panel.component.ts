4cda2cfa6f9aab5264b9ab001e26db9626ee6ca6
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-12-17 16:05:31 +0100

    1

--- src/app/settings-panel/settings-panel.component.ts
+++ src/app/settings-panel/settings-panel.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, ChangeDetectionStrategy, Component} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
@@ -11,12 +11,18 @@ import * as appActions from '../store/app.actions';
 import * as appSelectors from '../store/app.selectors';
 
 
-interface EnabledTechsForm {
+interface EnabledTechs {
   [tech: string]: FormControl<boolean|null>;
 }
 
+interface Colors {
+  backgroundColorHighlight: FormControl<string|null>;
+  colorHighlight: FormControl<string|null>;
+}
+
 interface SettingsForm {
-  enabledTechs: EnabledTechsForm;
+  colorsForm: FormGroup<Colors>;
+  enabledTechsForm: FormGroup<EnabledTechs>;
 }
 
 
@@ -31,16 +37,22 @@ export class SettingsPanelComponent implements AfterViewInit {
   readonly techs$: Observable<string[]> = this.store.select(appSelectors.selectTechs);
   private readonly settings: Settings = JSON.parse(localStorage.getItem('settings') ?? '');
   private enabledTechs: string[] = this.settings?.enabledTechs ?? [];
-  readonly enabledTechsForm = new FormGroup<EnabledTechsForm>({});
   
-  readonly enabledTechsForm$ = this.techs$.pipe(
+  readonly backgroundColorHighlight = new FormControl<string>('');
+  readonly colorHighlight = new FormControl<string>('');
+  private readonly colorsForm = new FormGroup<Colors>({
+    backgroundColorHighlight: this.backgroundColorHighlight, 
+    colorHighlight: this.colorHighlight,
+  });
+
+  readonly settingsForm$ = this.techs$.pipe(
     map(techs => {
-      const enabledTechsForm = new FormGroup<EnabledTechsForm>({});
+      const enabledTechsForm = new FormGroup<EnabledTechs>({});
       techs.forEach(tech => {
         const toggleControl = this.createToggleControl(this.enabledTechs, tech);
         enabledTechsForm.setControl(tech, toggleControl);
       });
-      return enabledTechsForm;
+      return new FormGroup({enabledTechsForm, colorsForm: this.colorsForm});
     }),
   );
 
@@ -54,13 +66,13 @@ export class SettingsPanelComponent implements AfterViewInit {
     this.enabledTechs = settings?.enabledTechs ?? this.enabledTechs;
   }
 
-  close(form: FormGroup<EnabledTechsForm>): void {
+  close(form: FormGroup<SettingsForm>): void {
     this.rightSidePanelService.close();
-    this.setToggleControls(form);
+    this.setToggleControls(form.controls.enabledTechsForm);
   }
 
-  saveSettings(form: FormGroup<EnabledTechsForm>): void {
-    const enabledTechs = Object.entries(form.value)
+  saveSettings(form: FormGroup<SettingsForm>): void {
+    const enabledTechs = Object.entries(form.value.enabledTechsForm!)
       .filter(entry => entry[1])
       .map(entry => entry[0]);
 
@@ -73,7 +85,7 @@ export class SettingsPanelComponent implements AfterViewInit {
     return new FormControl<boolean>(isChecked);
   }
 
-  private setToggleControls(form: FormGroup<EnabledTechsForm>): void {
+  private setToggleControls(form: FormGroup<EnabledTechs>): void {
     this.techs.forEach(tech => {
       form.setControl(tech, this.createToggleControl(this.enabledTechs, tech));
     });
