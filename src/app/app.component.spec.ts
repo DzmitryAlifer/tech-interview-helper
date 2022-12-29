c671c23e0bec67d6c59b99996973a68dce49bd8a
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-12-29 12:45:26 +0100

    1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tech-interview-helper1'`, () => {
+  it(`should have as title 'tech-interview-helper'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tech-interview-helper1');
+    expect(app.title).toEqual('tech-interview-helper');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tech-interview-helper1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tech-interview-helper app is running!');
   });
 });
